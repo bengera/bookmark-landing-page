90617efff50f5e71e5814b9b5b94e5e5e88633ba
Ben Geraghty <61493854+bengera@users.noreply.github.com>
2024-06-17 17:06:24 +0700

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import htmlPurge from 'vite-plugin-purgecss';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [
+    htmlPurge(),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        // Define your entry points here
+        index: resolve(__dirname, 'index.html'),
+      
+      }
+    }
+  }
+});
