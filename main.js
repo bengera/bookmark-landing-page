f47ccd0531b19894303d2b9293f86269824a0da9
Ben <bengeraghty.bg@gmail.com>
2024-07-04 13:02:35 +0700

    add navigation overlay

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Testing line for JavaScript');
+const hamMenu = document.querySelector('.hamburger-menu');
+const navUL = document.querySelector('.nav');
+
+hamMenu.addEventListener('click',() => {
+    console.log('toggle-menu');
+    hamMenu.classList.toggle('active');
+    navUL.classList.toggle('show');
+    
+    
+});
