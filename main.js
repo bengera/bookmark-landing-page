5b349627572716cc3d998efd4d983a6be829fcfd
Ben <bengeraghty.bg@gmail.com>
2024-07-04 13:24:14 +0700

    add white logo toggle effect

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const hamMenu = document.querySelector('.hamburger-menu');
 const navUL = document.querySelector('.nav');
+const logo = document.querySelector('.logo');
 
 hamMenu.addEventListener('click',() => {
     console.log('toggle-menu');
     hamMenu.classList.toggle('active');
     navUL.classList.toggle('show');
+    logo.src = 'images/logo-bookmark-white.svg';
+    logo.style.zIndex = '40';
     
     
 });
