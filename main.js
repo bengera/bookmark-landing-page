1ef815cf5c16588f6516836bc9963d9a75646c5d
Ben <bengeraghty.bg@gmail.com>
2024-07-08 12:12:17 +0700

    add overlay div

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const hamMenu = document.querySelector('.hamburger-menu');
 const navUL = document.querySelector('.nav');
 const logo = document.querySelector('.logo');
+const overlay = document.querySelector('.overlay');
 
 hamMenu.addEventListener('click',() => {
     console.log('toggle-menu');
     hamMenu.classList.toggle('active');
     navUL.classList.toggle('show');
+    overlay.classList.toggle('show');
     if (hamMenu.classList.contains('active')) {
         logo.src = 'images/logo-bookmark-white.svg';
         logo.style.zIndex = '40';
