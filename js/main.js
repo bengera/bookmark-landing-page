18e5093da29a9000b2e45a0a5157552b38dbe644
Ben <bengeraghty.bg@gmail.com>
2024-08-14 17:52:03 +0700

    error form working

--- js/main.js
+++ js/main.js
@@ -70,8 +70,44 @@ tabs.forEach((tab) => {
 // CHECK EMAIL FIELD
 
 const form = document.getElementById('form');
+const emailField = document.querySelector('.email-field');
+const buttonContact = document.querySelector('.contact-btn');
+const iconError = document.querySelector('.i-error');
+const errorDropdown = document.querySelector('.error-dropdown');
 
-form.addEventListener('click', (e)=> {
-    e.preventDefault();
-    console.log('check input');
-})
+form.addEventListener('click', (e) => {
+    if (e.target === buttonContact) { 
+        e.preventDefault();
+        console.log('check input');
+        checkEmail();
+    }
+});
+
+function checkEmail() {
+    const email = document.getElementById('email');
+    const emailValue = email.value.trim();
+
+    if (emailValue === '') {
+        setErrorFor(email, 'Whoops, make sure it’s an email');
+    } else if  (!isEmail(emailValue)) 
+        setErrorFor(email, 'Whoops, make sure it’s an email');
+     else  {
+
+    }
+
+}
+
+
+function setErrorFor() {
+          
+     emailField.classList.add('active');
+     iconError.classList.add('active');
+     errorDropdown.classList.add('active');
+     buttonContact.classList.add('active');
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+
