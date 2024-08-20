ed8865d8b4b1848eef98c7e33af330475e6361ad
Ben <61493854+bengera@users.noreply.github.com>
2024-08-20 16:03:18 +0700

    add calc to fix features shape issues

--- js/features.js
+++ js/features.js
@@ -2,7 +2,7 @@ const tabs = document.querySelectorAll('.features__button');
 const image = document.getElementById('features-img');
 const heading = document.getElementById('features-heading');
 const text = document.getElementById('features-text');
-const shape = document.getElementById('shape');
+// const shape = document.getElementById('shape');
 
 import tab1 from '../images/illustration-features-tab-1.svg';
 import tab2 from '../images/illustration-features-tab-2.svg';
@@ -22,7 +22,7 @@ tabs.forEach((tab) => {
                 heading.innerText = "Bookmark in one click";
                 text.innerText = 'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.'
                 image.src = tab1;
-                shape.style.height = "200px";
+                // shape.style.height = "200px";
                                 
                 break;
 
@@ -30,7 +30,7 @@ tabs.forEach((tab) => {
                     heading.innerText = "Intelligent search";
                     text.innerText = 'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.'
                     image.src = tab2;
-                    shape.style.height = "240px";
+                    // shape.style.height = "240px";
                                         
                     break;
 
@@ -38,7 +38,7 @@ tabs.forEach((tab) => {
                         heading.innerText = "Share your bookmarks";
                         text.innerText = 'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.'
                         image.src = tab3;
-                        shape.style.height = "250px";
+                        // shape.style.height = "250px";
                         
                         break;    
 
